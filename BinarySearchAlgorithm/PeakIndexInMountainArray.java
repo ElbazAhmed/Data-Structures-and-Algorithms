5e7a31fd7bce0457244d5b0cff96aedb80069615
Ahmed Elbaz <105916740+ElbazAhmed@users.noreply.github.com>
2023-12-22 23:34:28 +0100

    Find the Peak Index In Mountain Array

--- BinarySearchAlgorithm/PeakIndexInMountainArray.java
+++ BinarySearchAlgorithm/PeakIndexInMountainArray.java
@@ -1,6 +1,8 @@
 package BinarySearchAlgorithm;
 
 public class PeakIndexInMountainArray {
+    //Given a mountain array arr, return the index i such that arr[0] < arr[1] < ... < arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1].
+    //You must solve it in O(log(arr.length)) time complexity.
     public static void main(String[] args) {
         int[] arr = {0,2,1,0};
         System.out.println(peakIndexInMountainArray(arr));
