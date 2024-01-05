1915a8ef8cd54b8e31e150487af96229c5b8f0d1
Ahmed Elbaz <105916740+ElbazAhmed@users.noreply.github.com>
2024-01-05 23:17:16 +0100

    Bubble Sort algorithm

--- Sorting_algorithms/BubbleSort.java
+++ Sorting_algorithms/BubbleSort.java
@@ -5,12 +5,13 @@ import java.util.Arrays;
 public class BubbleSort {
 
     public static void main(String[] args) {
-        int[] arr = {6,2,7,3,-1,5};
+        int[] arr = {1,2,3,-32,-9,33,4,5};
         bubbleSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
     static void bubbleSort(int[] arr){
+        boolean swap = false;
         for(int i=0;i<arr.length;i++){
             //for each step max will take it's perfect place
             for(int j=1;j<arr.length-i;j++){
@@ -19,7 +20,12 @@ public class BubbleSort {
                     int tmp = arr[j];
                     arr[j]=arr[j-1];
                     arr[j-1]=tmp;
+                    swap=true;
                 }
+            }
+            //if no swap take place in the first iteration it means that the array is already sorted
+            if(swap == false){
+                break;
             }
         }
     }
