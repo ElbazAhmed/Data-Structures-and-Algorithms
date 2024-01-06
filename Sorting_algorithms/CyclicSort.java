fe78ffead575c0ab670a42d92c81e48c5a5bcf8d
Ahmed Elbaz <105916740+ElbazAhmed@users.noreply.github.com>
2024-01-06 18:01:06 +0100

    Custom Sort String : add comments

--- Sorting_algorithms/CyclicSort.java
+++ Sorting_algorithms/CyclicSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class CyclicSort {
     //given an array of number from 1 to N that is not sorted
-    //we sort them using the cyclic sort 
+    //we sort them using the cyclic sort
     public static void main(String[] args) {
         int[] arr ={3,5,6,7,2,1,4};
         cyclicSort(arr);
