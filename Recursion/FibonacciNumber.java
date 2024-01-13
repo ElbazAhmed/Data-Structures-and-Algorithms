14b366c27076d5818d28a9d14823260a4f049bc2
ahmed elbaz <ahmedelbaz771@gmail.com>
2024-01-13 14:42:04 +0100

    Fibonacci Number

---
+++ Recursion/FibonacciNumber.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+public class FibonacciNumber {
+    //https://leetcode.com/problems/fibonacci-number
+    //The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence,
+    //such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+    //F(0) = 0, F(1) = 1
+    //F(n) = F(n - 1) + F(n - 2), for n > 1.
+    public static void main(String[] args) {
+        int n = 6;
+        System.out.println(fib(n));
+    }
+    static int fib(int n) {
+        if(n==0){
+            return 0;
+        }else if(n==1){
+            return 1;
+        }
+        return fib(n-1)+fib(n-2);
+
+    }
+}
