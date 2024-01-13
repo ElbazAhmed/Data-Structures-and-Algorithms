ffbded0c1c79e36ed6ce1c456fa6032fafe255d8
ahmed elbaz <ahmedelbaz771@gmail.com>
2024-01-13 15:01:15 +0100

    Fibonacci Number

--- Recursion/FibonacciNumber.java
+++ Recursion/FibonacciNumber.java
@@ -11,10 +11,9 @@ public class FibonacciNumber {
         System.out.println(fib(n));
     }
     static int fib(int n) {
-        if(n==0){
-            return 0;
-        }else if(n==1){
-            return 1;
+        //base condition 
+        if(n<2){
+            return n;
         }
         return fib(n-1)+fib(n-2);
 
