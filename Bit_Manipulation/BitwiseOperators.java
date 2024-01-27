10b7d42c3168272755b24b64c007e1b9b8405acd
ahmed elbaz <ahmedelbaz771@gmail.com>
2024-01-27 19:59:58 +0100

    check if a number is odd using the least significant bit

--- Bit_Manipulation/BitwiseOperators.java
+++ Bit_Manipulation/BitwiseOperators.java
@@ -8,6 +8,7 @@ public class BitwiseOperators {
 
     static boolean isOdd(int num){
         //an int is odd if the LSB (the least significant bit) is equal to 1
+        //if the LSB of num is equal to 1 the result of this (num & 1) is 1  
         return (num & 1) == 1;
     }
 
