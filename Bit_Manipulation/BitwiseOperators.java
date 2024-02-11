37b9112e2c7d732412d240f4b8e8285e3ee71aa4
ahmed elbaz <ahmedelbaz771@gmail.com>
2024-02-11 09:13:31 +0100

    Find the ith Bit of binary number

--- Bit_Manipulation/BitwiseOperators.java
+++ Bit_Manipulation/BitwiseOperators.java
@@ -3,13 +3,27 @@ package Bit_Manipulation;
 public class BitwiseOperators {
     public static void main(String[] args) {
         int num = 2;
-        System.out.println(isOdd(num));
+        //System.out.println(isOdd(num));
+        System.out.println(findithBit(101110101,2));
     }
 
+    //AND Operator
     static boolean isOdd(int num){
         //an int is odd if the LSB (the least significant bit) is equal to 1
-        //if the LSB of num is equal to 1 the result of this (num & 1) is 1  
+        //if the LSB of num is equal to 1 the result of this (num & 1) is 1
         return (num & 1) == 1;
+    }
+
+    //Shift left operator 
+    static int findithBit(int num, int ithBit){
+        //given a binary presentation of an integer find the ith bit of it
+        //the shift left operrator 1<<(ithBit-1) give us the binary presentation of Math.pow(2,ithBit-1)
+        //because a << b = a*Math.pow(2,b)
+        if((num & (1<<(ithBit-1))) == 0){
+            //if the result is 0 its means that the ith bit is 0
+            return 0;
+        }
+        return 1;
     }
 
 }
