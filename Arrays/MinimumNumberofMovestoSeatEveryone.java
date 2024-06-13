4b4b6c0074d623ce8f27c4bdd74545814ecf1efd
ahmed elbaz <ahmedelbaz771@gmail.com>
2024-06-13 15:17:24 +0100

    Solved The Minimum Number of Moves to Seat Everyone problem, Beats 97.35% Runtime, 65.16% Memory of users with Java.

---
+++ Arrays/MinimumNumberofMovestoSeatEveryone.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+import java.util.Arrays;
+public class MinimumNumberofMovestoSeatEveryone {
+    // https://leetcode.com/problems/minimum-number-of-moves-to-seat-everyone/
+    // There are n seats and n students in a room. You are given an array seats of length n, where seats[i] is the position of the ith seat.
+    // You are also given the array students of length n, where students[j] is the position of the jth student.
+    //You may perform the following move any number of times:
+    //    Increase or decrease the position of the ith student by 1 (i.e., moving the ith student from position x to x + 1 or x - 1)
+    //Return the minimum number of moves required to move each student to a seat such that no two students are in the same seat.
+    public int minMovesToSeat(int[] seats, int[] students) {
+        int count = 0;
+        Arrays.sort(seats);
+        Arrays.sort(students);
+
+        int length = seats.length;
+
+        for (int i = 0; i < length; i++) {
+            count += Math.abs(students[i] - seats[i]);
+        }
+
+        return count;
+    }
+}
